d086162f2d6ce750e85a05523029b64e0c0b3ad3
wjw <2117678336@qq.com>
2023-10-22 10:26:33 +0800

    +初始化项目
    环境node18.17.0、vite3；
    添加ts、jsx、vue-router、pinia、vitest、eslint、prettier

---
+++ vue3-ts-cms/env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+// 如果vue导入ts文件声明报错添加类型。
+// declare module "*.vue" {
+//   import { DefinedComponent } from "node_modules/@vue/test-utils/dist/types";
+//   const component: DefinedComponent
+//   export default component
+// }
